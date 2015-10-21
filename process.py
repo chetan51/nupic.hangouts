07ac82f7c44efd7fd77c26b028df0db4389fbdcc
Chetan Surpur <chetan51@gmail.com>
2015-10-21 13:57:34 -0700

    Added output format for unicorn

--- process.py
+++ process.py
@@ -23,6 +23,7 @@ def process(dataPath, outputDir):
 
     textFileName = "{0}_{1}.txt".format(conversationId, len(events))
     csvFileName = "{0}_{1}.csv".format(conversationId, len(events))
+    jsonFileName = "{0}_{1}.json".format(conversationId, len(events))
 
     with open(os.path.join(outputDir, textFileName), 'w') as outfile:
       outfile.write(json.dumps(conversation['conversation']['participant_data']))
@@ -39,6 +40,13 @@ def process(dataPath, outputDir):
         timestampRaw = datetime.datetime.fromtimestamp(int(timestampString))
         timestamp = timestampRaw.strftime('%Y-%m-%d %H:%M:%S.0')
         writer.writerow([timestamp, 1])
+
+    with open(os.path.join(outputDir, jsonFileName), 'w') as outfile:
+      for event in events:
+        timestampString = str(event['timestamp'])[:10]
+        timestamp = int(timestampString)
+        outfile.write(json.dumps([timestamp, 1]))
+        outfile.write("\n")
 
 
 
