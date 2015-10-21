89defbe4ef62157a463ce05b1dea8b9be6b9beb5
Chetan Surpur <chetan51@gmail.com>
2015-10-21 15:03:20 -0700

    Added plotting script

---
+++ plot.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import argparse
+from datetime import datetime
+import json
+
+from matplotlib import pyplot
+
+
+
+def plot(dataPath, resultsPath):
+  with open(dataPath, 'r') as dataFile:
+    with open(resultsPath, 'r') as resultsFile:
+
+      timestamps = []
+      messages = []
+      anomalyScores = []
+
+      while True:
+        dataRow = dataFile.readline()
+        resultRow = resultsFile.readline()
+
+        if not dataRow or not resultRow:
+          break
+
+        data = json.loads(dataRow)
+        result = json.loads(resultRow)
+
+        timestamps.append(datetime.fromtimestamp(data[0]))
+        messages.append(data[1])
+        anomalyScores.append(result[1])
+
+      pyplot.subplot(2, 1, 1)
+      pyplot.title("Messages")
+      pyplot.plot(timestamps, messages)
+
+      pyplot.subplot(2, 1, 2)
+      pyplot.title("Anomaly Score")
+      pyplot.plot(timestamps, anomalyScores)
+
+      pyplot.show()
+
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('data', metavar='/path/to/data.json', type=str)
+  parser.add_argument('results', metavar='/path/to/results.json', type=str)
+
+  args = parser.parse_args()
+
+  plot(args.data, args.results)
