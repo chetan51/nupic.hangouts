9a74b7236c709e0adb04ff50b7e5d783bb7164b1
Chetan Surpur <chetan51@gmail.com>
2015-10-21 14:15:01 -0700

    Add data conversion script

---
+++ convert_unicorn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import csv
+from datetime import datetime
+import json
+import os
+import sys
+import time
+
+
+
+def convert(dataPath, outputPath):
+  with open(dataPath, 'r') as infile:
+    reader = csv.reader(infile)
+
+    for _ in xrange(3):
+      reader.next()
+
+    with open(outputPath, 'w') as outfile:
+      for row in reader:
+        timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.0')
+        timestamp = int(time.mktime(timestamp.timetuple()))
+        outfile.write(json.dumps([timestamp, 1]))
+        outfile.write("\n")
+
+
+
+if __name__ == "__main__":
+  convert(sys.argv[1], sys.argv[2])
